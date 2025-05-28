6d9fe5a7a653a3eea301ec426b8506b8b9f6d2a7
Geeta1178 <geeta.narayan10@gmail.com>
2025-05-27 21:06:21 -0500

    practice test1

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-
+import data
+import helpers
 class TestUrbanRoutes:
+    def test_set_route(self):
+        # Add in S8
+        pass
+    def test_select_plan(self):
+    def test_fill_phone_number(self):
+    def test_fill_card():
+    def test_comment_for_driver():
+    def test_order_blanket_and_handkerchiefs():
+    def test_order_2_ice_creams():
+    def test_car_search_model_appears():
+
+
